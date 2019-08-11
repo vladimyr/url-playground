0197c61fd2e1f73b266e90640b36a3b287b35255
Dario Vladovic <d.vladimyr@gmail.com>
2019-08-11 02:09:38 +0200

    Make playground live :rocket:

--- index.js
+++ index.js
@@ -5,8 +5,9 @@
  * - [Different results in parsing hostname/origin with custom uri schemes](https://github.com/jsdom/whatwg-url/issues/110)
  */
 
-import html from 'nanohtml';
 import * as whatwg from 'whatwg-url';
+import html from 'nanohtml';
+import { throttle } from 'throttle-debounce';
 import ObjectInspector from 'Inspector-JSON';
 
 const $output = document.querySelector('.inspectors');
@@ -18,14 +19,39 @@ $output.appendChild(inspector({
 }));
 
 function inspector({ url } = {}) {
-  return html`
+  let actual, expected;
+
+  const inspector = html`
     <div class="inspector">
-      <p class="code">new URL(${url})</p>
-      ${dump({ value: new URL(url), className: 'actual' })}
+      <p class="code">new URL('<span oninput=${throttle(350, onInput)} contenteditable="true">${url}</span>')</p>
+      ${(actual = dump({ value: new URL(url), className: 'actual' }))}
       <p class="code">// expected output according to <a href="https://url.spec.whatwg.org/" target="_blank">url.spec.whatwg.org</a></p>
-      ${dump({ value: new whatwg.URL(url), className: 'expected' })}
+      ${(expected = dump({ value: new whatwg.URL(url), className: 'expected' }))}
     </div>
   `;
+
+  return inspector;
+
+  function onInput(e) {
+    const url = e.target.textContent;
+
+    try {
+      // eslint-disable-next-line no-new
+      new URL(url);
+    } catch (err) {
+      actual.innerHTML = 'Invalid URL';
+      expected.innerHTML = 'Invalid URL';
+      return;
+    }
+
+    const newActual = dump({ value: new URL(url), className: 'actual' });
+    inspector.replaceChild(newActual, actual);
+    actual = newActual;
+
+    const newExpected = dump({ value: new whatwg.URL(url), className: 'expected' });
+    inspector.replaceChild(newExpected, expected);
+    expected = newExpected;
+  }
 }
 
 function dump({ value, config = { collapsed: false }, className = '' } = {}) {
